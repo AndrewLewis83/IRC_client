720c923705eee8216158a06851ecb8f983d0783a
Hunter-Culler <43865381+Hunter-Culler@users.noreply.github.com>
2020-02-17 13:16:34 -0700

    modified incoming and outgoing
    Outgoing now checks for specific commands, sending the message if none are found. Incoming now checks the received message against a list of blocked users, and only prints the message if no matches are found.

--- client.py
+++ client.py
@@ -3,30 +3,55 @@ import os
 import random
 import threading
 
+
 def outgoing():
-	while True:
-		global stop_threads
-		message = input(f"{nickname} > ")
-		if message:
-			if message == "exit":
-				irc.close()
-				stop_threads = True
-			else:    
-				irc.send(channel, message)
-		if stop_threads:
-			break
-    
+    while True:
+        global stop_threads
+        message = input(f"{nickname} > ")
+
+        # broke up the message into whitespace, with the 1st element being put into a variable called cmd
+        if len(message) != 0:
+            cmd = message.split()[0]
+
+        # if any of these commands are recognized it performs them, otherwise just sends the message.
+        if message:
+            # only changed which variable is being compared to string "exit"
+            if cmd == "exit":
+                irc.close()
+                stop_threads = True
+            # command for blocking a user. Assumes the user to be blocked is the 2nd argument of the message
+            elif cmd == "/block":
+                global blocklist
+                blocklist.append(message.split()[1])
+            # command for unblocking a user. Assumes the blocked user is the 2nd argument of the message
+            elif cmd == "/unblock":
+                blocklist.remove(message.split()[1])
+            else:
+                irc.send(channel, message)
+        if stop_threads:
+            break
+
+
 def incoming():
+    while True:
+        global stop_threads
+        global blocklist
+        blocked = False
+        text = irc.get_text()
+        # checks if any mention of a blocked user is in the message. If so blocks the message.
+        # this approach is overkill, and I will narrow it down to only messages sent by blocked users
+        for i in blocklist:
+            if i in text:
+                blocked = True
+        # if no mention of a blocked user in message, prints the message.
+        if not blocked:
+            print("\n", text)
 
-	while True:
-		global stop_threads
-		text = irc.get_text()
-		print("\n", text)
-		
-		if stop_threads:
-			break
-	
+        if stop_threads:
+            break
 
+
+blocklist = []  # list of blocked users
 channel = input("Enter channel name:> ")
 server = "irc.freenode.net"
 nickname = input("Enter Nick:> ")
