c39d88aba021b21b24d02c81a7010dd140d29d8d
Isaiah Haley <Isaiah.Haley.0@hotmail.com>
2020-02-19 06:27:30 -0700

    fixed formating
    reformatted text to match format wanted by pylint

--- filter.py
+++ filter.py
@@ -1,24 +1,24 @@
 import re
 
 #will also be used for handling messages from multiple servers
-class message:
+class Message:
     def __init__(self, inmessage):
         self.__message = inmessage.split(" ")
 
-    def getMessage(self):
+    def get_message(self):
         return self.__message
 
-    def setMessage(self, msg):
+    def set_message(self, msg):
         self.__message = msg
 
     def msg_to_string(self):
-        outMsg = ""
-        for x in self.__message:
-            outMsg += x + " "
-        return outMsg
+        out_msg = ""
+        for word in self.__message:
+            out_msg += word + " "
+        return out_msg
 
 
-class filter:
+class Filter:
     # need to change how replacement is handled
     __replacement = ['grinch', 'oobleck', 'sneetches', 'skritz', 'zillow', 'thneeds', 'wocket', 'squitsch']
 
@@ -26,40 +26,39 @@ class filter:
         self.__fl = {}
 
     # adds a new word to be filtered
-    def filterWord(self, fw):
-        if len(self.__fl) > 0 and fw not in self.__fl:
-            self.__fl[fw] = self.__replacement.pop(0)
+    def filter_word(self, filter_word):
+        if len(self.__fl) > 0 and filter_word not in self.__fl:
+            self.__fl[filter_word] = self.__replacement.pop(0)
         else:
-            self.__fl[fw] = self.__replacement.pop(0)
+            self.__fl[filter_word] = self.__replacement.pop(0)
 
     # checks the incomming message against the filter list
-    def filterMsg(self, msg):
-        i = 0
-        for x in msg:
-            tststr = x
-            for y in self.__fl:
-                regmsg = r".*" + y + ".*"
+    def filter_msg(self, msg):
+        counter = 0
+        for incomming_text in msg:
+            tststr = incomming_text
+            for filter_text in self.__fl:
+                regmsg = r".*" + filter_text + ".*"
                 result = re.match(regmsg, tststr, re.IGNORECASE)
                 if result:
-                    msg[i] = self.__fl[y]
+                    msg[counter] = self.__fl[filter_text]
                     #needs to also handle concatinating punctuation and other digets back to the word
-            i = i + 1
+            counter = counter + 1
         return msg
 
 
 class Handler:
 
     def __init__(self):
-        self.__numFiltered = 0
-        self.filt = filter()
+        self.filt = Filter()
 
     # handles a incomming message
     def handle(self, msg):
         if type(msg) is str:
             args = msg.split(" ")
             if str(args[0]) == '/f':
-                self.filt.filterWord(args[1])
+                self.filt.filter_word(args[1])
             else:
-                tMsg = message(msg)
-                tMsg.setMessage(self.filt.filterMsg(tMsg.getMessage()))
-                return tMsg.msg_to_string()
+                temp_message = Message(msg)
+                temp_message.set_message(self.filt.filter_msg(temp_message.get_message()))
+                return temp_message.msg_to_string()
