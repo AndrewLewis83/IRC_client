7fe7d98df0cb560000bcd45032b24f9fc77ad0b8
Hunter-Culler <43865381+Hunter-Culler@users.noreply.github.com>
2020-04-02 13:33:23 -0600

    list implementation

--- client.py
+++ client.py
@@ -22,10 +22,10 @@ def outgoing():
             # command for blocking a user. Assumes the user to be blocked is the 2nd argument of the message
             elif cmd == "/block":
                 global blocklist
-                blocklist[message.split()[1]] = True
+                blocklist.append(message.split()[1])
             # command for unblocking a user. Assumes the blocked user is the 2nd argument of the message
             elif cmd == "/unblock":
-                blocklist[message.split()[1]] = False
+                blocklist.remove(message.split()[1])
             else:
                 irc.send(channel, message)
         if stop_threads:
@@ -44,16 +44,20 @@ def incoming():
             # user is always before the ! symbol
             user = text.split('!')
         user[0].strip()
-
+        # checks if any mention of a blocked user is in the message. If so blocks the message.
+        # this approach is overkill, and I will narrow it down to only messages sent by blocked users
+        for i in blocklist:
+            if i in user[0]:
+                blocked = True
         # if no mention of a blocked user in message, prints the message.
-        if not blocklist[user[0]]:
+        if not blocked:
             print("\n", user[0], ": ", message[len(message) - 1])
 
         if stop_threads:
             break
 
 
-blocklist = {}  # list of blocked users
+blocklist = []  # list of blocked users
 channel = input("Enter channel name:> ")
 server = "irc.freenode.net"
 nickname = input("Enter Nick:> ")
